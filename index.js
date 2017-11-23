e677f5a663a9a2b2566ed14dee492d4541f92568
Cameron Meek <meekca1@gmail.com>
2017-11-23 03:40:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(string) {
-  console.log(string.toUpperCase)
+function logShout('HELLO') {
+  console.log('HELLO'.toUpperCase)
 }
 
 function logWhisper(string) {
